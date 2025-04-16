94e441d70e7b0a53d1998d62e80f818e792cf66f
RAMARAJA-Dev <nramarajait@gmail.com>
2025-04-16 22:42:03 +0530

    lib code

---
+++ LibraryAdd.py
@@ -0,0 +1,26 @@
+#LibraryAdd.py-----File Name and Module Name
+import oracledb as orc
+def addbook():
+    while(True):
+        try:
+            con=orc.connect("Raja/9494293339@localhost/orcl")
+            cur=con.cursor()
+            #Accept Book Details from KBD
+            print("---------------------------------------------")
+            bno=int(input("Enter Book Number:"))
+            bname=input("Enter Book Name:")
+            price=float(input("Enter Book Price:"))
+            pub=input("Enter Publication Name:")
+            cur.execute("insert into library values(%d,'%s',%f,'%s')" %(bno,bname,price,pub))
+            con.commit()
+            print("{} Book Details Saved Successfully".format(cur.rowcount))
+            print("---------------------------------------------")
+            ch=input("Do u want to Insert Another Book Details(yes/no):")
+            if(ch.lower()=="no"):
+                print("Thc for this Program")
+                break
+        except orc.DatabaseError as db:
+            print("Problem in Oracle: ",db)
+        except ValueError:
+            print("Don't enter strs alnums and symbols for book number and price--try again")
+
---
+++ LibrayMenu.py
@@ -0,0 +1,13 @@
+#LibrayMenu.py<--------File Name and Module Name
+def menu():
+    print("*"*50)
+    print("\tLibrary Management System")
+    print("*" * 50)
+    print("\t\t1.Add a New Book")
+    print("\t\t2.Delete a Book LibraryAdd.py")
+    print("\t\t3.Update Book Details")
+    print("\t\t4.View Book Details")
+    print("\t\t5.View All Book Details")
+    print("\t\t6.Exit")
+    print("*" * 50)
+
